39ccdde34a0e054d49b8d4adf5645a2fd1bb9b98
CHeessoo <gabycho41@gmail.com>
2023-07-14 10:42:40 +0900

    20230714

--- 04_Control_Ex/src/ex01_branch/MainWrapper.java
+++ 04_Control_Ex/src/ex01_branch/MainWrapper.java
@@ -1,6 +1,7 @@
 package ex01_branch;
 
 import java.io.StreamCorruptedException;
+import java.net.Socket;
 
 import javax.swing.plaf.basic.BasicInternalFrameTitlePane.IconifyAction;
 
@@ -114,6 +115,7 @@ public class MainWrapper {
       pass = "불합격";
     }
     System.out.println(pass);
+    
   }
   
   // 4번 문제
@@ -128,17 +130,10 @@ public class MainWrapper {
     int price;
     
     switch (order) {
-    case "아메리카노":
-      price = 2000;
-      break;
-    case "카페라떼":
-      price = 2500;
-      break;
-    case "밀크티":
-      price = 3000;
-      break;
-    default:
-      price = 5000;
+    case "아메리카노": price = 2000; break;
+    case "카페라떼": price = 2500; break;
+    case "밀크티": price = 3000; break;
+    default: price = 5000;
     }
     
     System.out.println(order + "는 " +  price + "원 입니다.");
@@ -149,22 +144,51 @@ public class MainWrapper {
   // 5번 문제
   public static void ex05() {
     
-    //월에 따른 계절
-    //3 ~ 5 : 봄
-    //6 ~ 8 : 여름
-    //9 ~ 11: 가을
-    //12 ~ 2: 겨울
+    //월에 따른 계절    월 % 12
+    //3 ~ 5 : 봄        3 ~ 5
+    //6 ~ 8 : 여름      6 ~ 8
+    //9 ~ 11: 가을      9 ~ 11
+    //12 ~ 2: 겨울      0 ~ 2
     int month = 7;
     String season;  // "봄", "여름", "가을", "겨울"
     
-    switch (month) {
-    case 3: case 4: case 5:
-      season = "봄";
-      break;
 
-    default:
-      break;
+    if(3 <= month && month <= 5) {
+      season = "봄";
+    } else if (6 <= month && month <= 8) {
+      season = "여름";
+    } else if (9 <= month && month <= 11) {
+      season = "가을";
+    } else {
+      season = "겨울";
     }
+    System.out.println(month + "월은 " + season + "입니다.");
+    
+  }
+  
+  
+  // 5_2번 문제
+  public static void ex05_2() {
+    
+    //월에 따른 계절    월 % 12
+    //3 ~ 5 : 봄        3 ~ 5
+    //6 ~ 8 : 여름      6 ~ 8
+    //9 ~ 11: 가을      9 ~ 11
+    //12 ~ 2: 겨울      0 ~ 2
+    int month = 1;
+    int mod = month % 12; //월 % 12
+    String season;  // "봄", "여름", "가을", "겨울"
+    if (mod <= 2) {
+      season = "겨울";
+    } else if (mod <= 5) {
+      season = "봄";
+    } else if (mod <= 8) {
+      season = "여름";
+    } else {
+      season = "가을";
+    }
+
+    System.out.println(month + "월은 " + season + "입니다.");
     
   }
   
@@ -177,7 +201,34 @@ public class MainWrapper {
     //4 ~ 6 : 2분기
     //7 ~ 9 : 3분기
     //10 ~ 12 : 4분기
-    int m = 7;
+    int month = 7;
+    
+    if(month <= 3) {
+      System.out.println("1분기");
+    } else if (month <= 6) {
+      System.out.println("2분기");
+    } else if (month <= 9) {
+      System.out.println("3분기");
+    } else {
+      System.out.println("4분기");
+    }
+    
+  }
+  
+  
+  // 6_2번 문제
+  public static void ex06_2() {
+    
+    //월에 따른 분기        분기계산
+    //1 ~ 3 : 1분기         (month - 1) / 3 + 1
+    //4 ~ 6 : 2분기
+    //7 ~ 9 : 3분기
+    //10 ~ 12 : 4분기
+    int month = 7;
+    
+    System.out.println((month - 1) / 3 + 1 + "분기");
+    
+
     
   }
   
@@ -186,9 +237,22 @@ public class MainWrapper {
   public static void ex07() {
     
     //10일 후 요일은?
-    int day = 13;  // 13일은 목요일
+    int day = 14;  // 13일은 금요일
     int nDay = 10;  // 10일
     String weekname;  // "월", "화", "수", "목", "금", "토", "일"
+    
+    switch ((day += nDay) % 7) {
+    case 0: weekname = "금"; break;
+    case 1: weekname = "토"; break;
+    case 2: weekname = "일"; break;
+    case 3: weekname = "월"; break;
+    case 4: weekname = "화"; break;
+    case 5: weekname = "수"; break;
+    default: weekname = "목";
+    }
+    
+    System.out.println(day + "일은 " + weekname + "요일 입니다.");
+    
     
   }
   
@@ -197,7 +261,22 @@ public class MainWrapper {
   public static void ex08() {
     
     // 대소문자 변환 (구글링으로 아스키코드 검색 후 참고)
-    char ch = 'A';  // 임의의 대문자 또는 소문자
+    char ch = 'l';  // 임의의 대문자 또는 소문자
+    
+    if (ch >= 'A' && ch <= 'Z') {
+      ch += 32;   // ch = ch 32;
+    } else if (ch >= 'a' && ch <= 'z') {
+      ch -= 32;   // ch = ch -32;
+    }
+    
+    char ch1 = 'a' + 2;
+    
+    
+    
+    System.out.println(ch1);
+        
+    
+    System.out.println(ch);
     
   }
 
@@ -206,12 +285,14 @@ public class MainWrapper {
 //    ex01();
 //    ex02();
 //    ex03();
-    ex03_2();
+//    ex03_2();
 //    ex04();
 //    ex05();
+//    ex05_2();
 //    ex06();
+//    ex06_2();
 //    ex07();
-//    ex08();
+    ex08();
 
   }
 
